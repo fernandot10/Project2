74b7f41c749768132e6f7df4f722c4acf93f5ca5
[Sarah Lang] <[lynnlang98@gmail.com]>
2023-01-26 21:31:58 -0500

    POST route for sign up

--- controller/api/user-routes.js
+++ controller/api/user-routes.js
@@ -2,6 +2,20 @@ const router = require('express').Router();
 const { User } = require('../../models');
 
 // POST route for signing up
+router.post('/', async (req, res) => {
+  try {
+    const userData = await User.create(req.body);
+
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+
+      res.status(200).json(userData);
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
 
 // POST route for logging in
 router.post('/login', async (req, res) => {
