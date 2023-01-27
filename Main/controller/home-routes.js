342409fd31965b2aa313404581ea82af4ce5ad0c
ALYSSA_MAPLES <raynemaples@gmail.com>
2023-01-26 19:33:59 -0500

    Started routes in home-routes file

--- Main/controller/home-routes.js
+++ Main/controller/home-routes.js
@@ -4,23 +4,49 @@ const withAuth = require('../utils/auth');
 // GET Route for Homepage
 router.get('/', (req, res) => {
   // redirects user to login page if not signed in
-
+  if (req.session.loggedIn) {
+    res.redirect('/dashboard');
+    return;
+  } else {
+    res.redirect('/login');
+    return;
+  }
   // otherwise redirects user to dashboard
 });
 
 // GET Route for Dashboard that shows all albums reviewed so far
 router.get('/dashboard', withAuth, (req, res) => {
+  try {
+    // variable for getting all album reviews from Model
 
+    // render albums on page
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 // GET route for showing a specific review
 router.get('/reviews/:id', withAuth, (req, res) => {
+  try {
+    // variable for getting single album review by id
 
+    // render album on page
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 // POST route for creating a new review
-router.post('reviews', withAuth, (req, res) => {
+router.post('/reviews', withAuth, (req, res) => {
+  try {
+    // variable for album review data - gets data for album, then data for review
 
+    // show 200 status if successful
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 // Redirects users if already logged in
