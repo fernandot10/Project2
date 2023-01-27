d8fc0365c467ba6a68827fc25324394a49d384f8
ALYSSA_MAPLES <raynemaples@gmail.com>
2023-01-26 19:27:14 -0500

    Moved review routes to the home routes file

--- Main/controller/home-routes.js
+++ Main/controller/home-routes.js
@@ -1,13 +1,32 @@
 const router = require('express').Router();
 const withAuth = require('../utils/auth');
 
-// Homepage Route - Gives user option to login or sign-up. 
+// GET Route for Homepage
+router.get('/', (req, res) => {
+  // redirects user to login page if not signed in
 
+  // otherwise redirects user to dashboard
+});
+
+// GET Route for Dashboard that shows all albums reviewed so far
+router.get('/dashboard', withAuth, (req, res) => {
+
+});
+
+// GET route for showing a specific review
+router.get('/reviews/:id', withAuth, (req, res) => {
+
+});
+
+// POST route for creating a new review
+router.post('reviews', withAuth, (req, res) => {
+
+});
 
 // Redirects users if already logged in
 router.get('/login', (req, res) => {
     if (req.session.loggedIn) {
-      res.redirect('/api/reviews');
+      res.redirect('/dashboard');
       return;
     }
   
