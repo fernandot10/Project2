f6c367038121f64182acc3dd5700f3146effbdc1
ALYSSA_MAPLES <raynemaples@gmail.com>
2023-01-26 20:02:00 -0500

    Fixed local host

--- controller/api/index.js
+++ controller/api/index.js
@@ -1,9 +1,7 @@
 const router = require('express').Router();
 
-const reviewRoutes = require('./review-routes');
 const userRoutes = require('./user-routes')
 
-router.use('/reviews', reviewRoutes);
 router.use('/users', userRoutes);
 
 module.exports = router;
