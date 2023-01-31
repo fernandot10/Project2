6817fcfeeebd4c4b23b740edbe41f0c570388689
fernandot10 <113951371+fernandot10@users.noreply.github.com>
2023-01-31 18:23:57 -0500

    review model

---
+++ models/Review.js
@@ -0,0 +1,41 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+
+class Review extends Model { }
+
+Review.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        AlbumId: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        UserId: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        rating: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        comment: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        }
+    },
+
+    {
+        sequelize,
+        timestamps: false,
+        underscored: true,
+        modelName: 'review'
+    }
+);
+
+module.exports = Review;
