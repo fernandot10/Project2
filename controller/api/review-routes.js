125710c2fdbbe8a4441feaed8d2bc53e10aa18ca
ALYSSA_MAPLES <raynemaples@gmail.com>
2023-01-31 18:45:49 -0500

    Separated route files

---
+++ controller/api/review-routes.js
@@ -0,0 +1,75 @@
+
+// GET route for showing all reviews 
+router.get('/reviews', withAuth, async (req, res) => {
+    try {
+      // variable for getting all reviews in general
+      const allReviews = await Reviews.findAll({
+        include: [{ model: Albums }],
+      });
+  
+      const reviews = allReviews.map((project) => project.get({ plain: true }));
+      // rendering reviews on page
+      res.render('reviews', { reviews });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  
+  // GET route for showing a specific review
+  router.get('/reviews/:id', withAuth, async (req, res) => {
+    try {
+      const allReviews = await Reviews.findByPk(req.params.id, {
+        include: [{ model: Albums }],
+      });
+  
+      // serialize data so templates can read
+      const review = allReviews.map((project) => project.get({ plain: true }));
+  
+      // pass serialized data into handlebars
+      res.render('review', { review });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  
+  // POST route for creating a new review
+  router.post('/reviews', withAuth, async (req, res) => {
+    try {
+      // variable for album review data - gets data for album, then data for review
+      const reviewData = await Reviews.create({
+        rating: req.body.rating,
+        comment: req.body.comment,
+        album_id: req.body.album_id,
+        user_id: req.body.user_id,
+      });
+  
+      // show 200 status if successful
+      res.status(200).json(reviewData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  
+  // DELETE route for deleting a review
+  router.delete('/reviews/:id', withAuth, async (req, res) => {
+    try {
+      // use .destroy
+      const reviewData = await Reviews.destroy({
+        where: {
+          id: req.params.id,
+        },
+      });
+  
+      //if no id found gives 404
+      if (!reviewData) {
+        res.status(404).json('No review found with that id!');
+        return;
+      }
+  
+      // status 200 if successful res.status(200).json(reviewData);
+  } catch (err) {
+    res.status(500).json(err)
+  }
+});
+
+module.exports = router;
