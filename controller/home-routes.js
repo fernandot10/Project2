8db388bc149b8c5be535dcedd513f77de064cef6
ALYSSA_MAPLES <raynemaples@gmail.com>
2023-01-30 20:28:42 -0500

    Edited home routes

--- controller/home-routes.js
+++ controller/home-routes.js
@@ -3,7 +3,7 @@ const { Album, Reviews, Users } = require('../models/');
 const withAuth = require('../utils/auth');
 
 // GET Route for Homepage
-router.get('/', withAuth, (req, res) => {
+router.get('/', withAuth, async (req, res) => {
   try {
     const albumData = await Album.findAll({
       limit: 10,
@@ -55,7 +55,7 @@ router.get('/albums/:id', withAuth, async (req, res) => {
 })
 
 // GET route for showing all reviews 
-router.get('/reviews', withAuth, (req, res) => {
+router.get('/reviews', withAuth, async (req, res) => {
   try {
     // variable for getting all reviews in general
     const allReviews = await Reviews.findAll({
@@ -71,7 +71,7 @@ router.get('/reviews', withAuth, (req, res) => {
 });
 
 // GET route for showing a specific review
-router.get('/reviews/:id', withAuth, (req, res) => {
+router.get('/reviews/:id', withAuth, async (req, res) => {
   try {
     const allReviews = await Reviews.findByPk(req.params.id, {
       include: [{ model: Albums }],
@@ -117,7 +117,7 @@ router.delete('/reviews/:id', withAuth, async (req, res) => {
 
     //if no id found gives 404
     if (!reviewData) {
-      res.status(404).json({'No review found with that id!'});
+      res.status(404).json('No review found with that id!');
       return;
     }
 
