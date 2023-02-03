6f7555e45c34ebdc1ef9a8815af6c1e3b97f9875
ALYSSA_MAPLES <raynemaples@gmail.com>
2023-02-02 19:30:01 -0500

    Implementing uploader in html

--- public/javascript/review.js
+++ public/javascript/review.js
@@ -5,22 +5,24 @@ const uploader = Uploader({
   apiKey: process.env.API_KEY;
 });
 
-uploader.open({ 
-    "maxFileCount": 1,
-    "editor": {
-        "images": {
-          "crop": true,
-          "cropRatio": 1,
-          "cropShape": "circ"
-        }
-    },
-    "mimeTypes": [
-        "image/jpeg"
-    ],
-    "multi": false,
-
-     }).then(
-    error => {
-      alert(error);
-    }
-  );
+function uploadFile() {
+    uploader.open({ 
+        "maxFileCount": 1,
+        "editor": {
+            "images": {
+            "crop": true,
+            "cropRatio": 1,
+            "cropShape": "circ"
+            }
+        },
+        "mimeTypes": [
+            "image/jpeg"
+        ],
+        "multi": false
+    }).then(
+      files => alert(files.length === 0 
+                     ? "No file selected!" 
+                     : `File uploaded!\n\n${files[0].fileUrl}`),
+      error => alert(error)
+    );  
+}
