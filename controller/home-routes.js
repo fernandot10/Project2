db44494d18e0a960d2be3e75087e4ec99a2286cf
bmfoster5 <foster.brandon96@gmail.com>
2023-02-02 20:40:49 -0500

    get route for specific album by id

--- controller/home-routes.js
+++ controller/home-routes.js
@@ -1,18 +1,48 @@
 const router = require('express').Router();
-const { Album } = require('../models/');
+const { Album, User  } = require('../models/');
 const withAuth = require('../utils/auth');
 
 // GET Route for Homepage
 router.get('/', withAuth, async (req, res) => {
   try {
     const albumData = await Album.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['email']
+        },
+      ],
       limit: 10,
       order: [['id', 'DESC']]
     });
 
+    // Serialize data
     const albums = albumData.map((project) => project.get({ plain: true }));
 
+    // Pass serialized data and session flag into template
     res.render('homepage', { albums });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/album/:id', async (req, res) => {
+  try {
+    const albumData = await Album.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ['email'],
+        },
+      ],
+    });
+
+    const Album = albumData.get({ plain: true });
+
+    res.render('album', {
+      ...project,
+      logged_in: req.session.logged_in
+    });
   } catch (err) {
     res.status(500).json(err);
   }
@@ -34,6 +64,5 @@ router.get('/signup', (req, res) => {
   }
   res.render('signup');
 });
-
 
 module.exports = router;
--- seeds/seed.js
+++ seeds/seed.js
@@ -1,13 +1,12 @@
 const sequelize = require('../config/connection');
-const { Users, Album } = require('../models');
-
+const { User, Album } = require('../models');
 const albumData = require('./albumData.json');
 const usersData = require('./usersData.json');
 
 const seedDatabase = async () => {
     await sequelize.sync({ force: true });
 
-    const users = await Users.bulkCreate(usersData, {
+    const users = await User.bulkCreate(usersData, {
         individualHooks: true,
         returning: true,
     });
