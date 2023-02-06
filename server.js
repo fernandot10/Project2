d3adb9c595e456a981205148b0b1533cb4437706
ALYSSA_MAPLES <raynemaples@gmail.com>
2023-02-06 15:25:59 -0500

    Small change

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.engine('handlebars', hbs.engine);
-app.set('Views', path.join(__dirname, 'Views'));
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'handlebars');
 
 app.use(routes);
